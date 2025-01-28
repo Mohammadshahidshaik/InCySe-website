69acebacad9ec6dd3dbffbdbd069cb0186a00314
Mohammad Shahid Shaik <124763182+Mohammadshahidshaik@users.noreply.github.com>
2025-01-27 19:59:09 -0600

    js

--- js/script.js
+++ js/script.js
@@ -20,26 +20,3 @@ document.getElementById("login").addEventListener("submit", function (e) {
     alert("Invalid username or password. Please try again!");
   }
 });
-
-
-
-document.getElementById("dashboardlogin").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const password = document.getElementById("password").value;
-
-  // Hardcoded credentials
-  const credentials = [
-    { password: "admin123" }
-  ];
-
-  const validUser = credentials.find(
-    (user) => user.password === password
-  );
-
-  if (validUser) {
-    window.location.href = "../pages/dashboard.html"; // Redirect to employee.html
-  } else {
-    alert("Invalid password. Please try again!");
-  }
-});
